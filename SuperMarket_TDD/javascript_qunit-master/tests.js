5290e07c47862a1af128d8d3290b25941c7b429a
JunichiSato <sato440nomusuko@gmail.com>
2018-01-13 15:01:15 +0900

    11.複数個計算するテストをもう一つ修正する

--- SuperMarket_TDD/javascript_qunit-master/tests.js
+++ SuperMarket_TDD/javascript_qunit-master/tests.js
@@ -16,9 +16,11 @@ QUnit.module('スーパーの支払金額を計算する', function () {
         assert.equal(actual, 200);
     
     });
-    QUnit.test('商品番号7を1個で合計金額440を返す', function (assert) {
-        var actual = cart(7, 1);
-        assert.equal(actual, 440);
+    QUnit.test('商品番号7を3個で合計金額1320を返す', function (assert) {
+        var items = 7;
+        var itemCount = 3;
+        var actual = cart(items, itemCount);
+        assert.equal(actual, 1320);
     }); 
 });
 
